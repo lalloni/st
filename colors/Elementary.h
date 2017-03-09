f13741c2bc2c619c0f7e3ae9b209eed005f52d2e
Pablo Lalloni <plalloni@gmail.com>
2017-03-09 18:38:32 -0300

    Add color schemes

---
+++ colors/Elementary.h
@@ -0,0 +1,34 @@
+/* Suckless ST config.h Fragment
+ * Generated with Hodler (http://github.com/matttproud/hodler)
+ */
+static const char *colorname[] = {
+	"#242424",		/* 0: ANSI Color 0 */
+	"#d61b15",		/* 1: ANSI Color 1 */
+	"#59a413",		/* 2: ANSI Color 2 */
+	"#fdb40b",		/* 3: ANSI Color 3 */
+	"#053a8c",		/* 4: ANSI Color 4 */
+	"#e40038",		/* 5: ANSI Color 5 */
+	"#2594e0",		/* 6: ANSI Color 6 */
+	"#eeeeee",		/* 7: ANSI Color 7 */
+	"#4a4a4a",		/* 8: ANSI Color 8 */
+	"#fb1b18",		/* 9: ANSI Color 9 */
+	"#6ac118",		/* 10: ANSI Color 10 */
+	"#fdc70e",		/* 11: ANSI Color 11 */
+	"#0855fe",		/* 12: ANSI Color 12 */
+	"#fb004f",		/* 13: ANSI Color 13 */
+	"#3ea7fc",		/* 14: ANSI Color 14 */
+	"#8c00eb",		/* 15: ANSI Color 15 */
+	[255] = 0,
+	[256] = "#181818",		/* 256: Background */
+	[257] = "#eeeeee",		/* 257: Foreground */
+	[258] = "#bbbbbb",		/* 258: Cursor */
+	[259] = "#ffffff",		/* 259: Cursor Text */
+	/* No support for text highlight coloring; would be #000000. */
+	/* No support for highlight coloring; would be #b4d5ff. */
+	/* No support for bold coloring; would be #ffffff. */
+};
+
+static unsigned int defaultfg  = 257;
+static unsigned int defaultbg  = 256;
+static unsigned int defaultcs  = 258;
+static unsigned int defaultrcs = 259;
---
+++ colors/Pandora.h
@@ -0,0 +1,34 @@
+/* Suckless ST config.h Fragment
+ * Generated with Hodler (http://github.com/matttproud/hodler)
+ */
+static const char *colorname[] = {
+	"#000000",		/* 0: ANSI Color 0 */
+	"#ff4242",		/* 1: ANSI Color 1 */
+	"#74af68",		/* 2: ANSI Color 2 */
+	"#ffad29",		/* 3: ANSI Color 3 */
+	"#338f86",		/* 4: ANSI Color 4 */
+	"#9413e5",		/* 5: ANSI Color 5 */
+	"#23d7d7",		/* 6: ANSI Color 6 */
+	"#e1e1e1",		/* 7: ANSI Color 7 */
+	"#3e5548",		/* 8: ANSI Color 8 */
+	"#ff3242",		/* 9: ANSI Color 9 */
+	"#74cd68",		/* 10: ANSI Color 10 */
+	"#ffb929",		/* 11: ANSI Color 11 */
+	"#23d7d7",		/* 12: ANSI Color 12 */
+	"#ff37ff",		/* 13: ANSI Color 13 */
+	"#00ede1",		/* 14: ANSI Color 14 */
+	"#ffffff",		/* 15: ANSI Color 15 */
+	[255] = 0,
+	[256] = "#131d42",		/* 256: Background */
+	[257] = "#e1e1e1",		/* 257: Foreground */
+	[258] = "#43d58e",		/* 258: Cursor */
+	[259] = "#ffffff",		/* 259: Cursor Text */
+	/* No support for text highlight coloring; would be #82dffe. */
+	/* No support for highlight coloring; would be #2d37ff. */
+	/* No support for bold coloring; would be #67a571. */
+};
+
+static unsigned int defaultfg  = 257;
+static unsigned int defaultbg  = 256;
+static unsigned int defaultcs  = 258;
+static unsigned int defaultrcs = 259;
